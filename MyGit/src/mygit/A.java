9f39e37f9a24242ba676d237096ce5d53ce57322
lovab_000 <lovab_000@song>
2020-06-17 01:26:01 +0900

    branch B

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) throws Exception {
         System.out.println("2017301036 Baekjihyeon_Software_Homwork");
+        System.out.println("2017301036 Baekjihyeon_Software_Homwork");
+        System.out.println("2017301036 Baekjihyeon_Software_Homwork");
 	}
 }
--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -3,5 +3,6 @@ package mygit;
 public class B {
 	public static void main(String[] args) throws Exception {
         System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n");
+        System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n");
     }
 }
