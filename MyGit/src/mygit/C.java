9bab127945f4307a54dc2f8afb6f3289fdf3eaf5
lovab_000 <lovab_000@song>
2020-06-17 01:28:29 +0900

    branch C

--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -3,5 +3,6 @@ package mygit;
 public class C {
 	public static void main(String[] args) throws Exception {
         System.out.println("Computer");
+        System.out.println("Computer");
     }
 }
